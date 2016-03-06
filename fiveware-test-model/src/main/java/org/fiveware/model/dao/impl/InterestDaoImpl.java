7a6c1c8a32dacda648b65231d60e8b646fe13af4
Andre Almeida <almeida.andref@gmail.com>
2016-03-06 14:07:10 -0300

    adiçao das anotaçoes aos daos

--- fiveware-test-model/src/main/java/org/fiveware/model/dao/impl/InterestDaoImpl.java
+++ fiveware-test-model/src/main/java/org/fiveware/model/dao/impl/InterestDaoImpl.java
@@ -2,7 +2,9 @@ package org.fiveware.model.dao.impl;
 
 import org.fiveware.model.dao.InterestDao;
 import org.fiveware.model.entity.Interest;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class InterestDaoImpl extends BaseDaoImpl<Interest> implements InterestDao {
 
 }
--- fiveware-test-model/src/main/java/org/fiveware/model/dao/impl/UserDaoImpl.java
+++ fiveware-test-model/src/main/java/org/fiveware/model/dao/impl/UserDaoImpl.java
@@ -2,7 +2,9 @@ package org.fiveware.model.dao.impl;
 
 import org.fiveware.model.dao.UserDao;
 import org.fiveware.model.entity.User;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class UserDaoImpl extends BaseDaoImpl<User> implements UserDao {
 
 }
